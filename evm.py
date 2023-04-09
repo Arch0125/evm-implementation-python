87a8e92022473b20333ed4cc192898a9006adc99
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-04-10 01:50:20 +0530

    fix: execute function, add: sub & div opcode

--- evm.py
+++ evm.py
@@ -12,6 +12,8 @@ class EVM:
             0x00: (self.op_stop, 0),
             0x01: (self.op_add, 3),
             0x02: (self.op_mul, 5),
+            0x03: (self.op_sub, 3),
+            0x04: (self.op_div, 5),
             0x60: (self.op_push1, 3),
         }
 
@@ -29,6 +31,19 @@ class EVM:
 
     def op_mul(self, bytecode):
         self.stack.append((self.stack.pop() * self.stack.pop()) % 2**256)
+        return False
+    
+    def op_sub(self, bytecode):
+        self.stack.append((self.stack.pop() - self.stack.pop()) % 2**256)
+        return False
+    
+    def op_div(self, bytecode):
+        n1 = self.stack.pop()
+        n2 = self.stack.pop()
+        if n2 == 0:
+            self.stack.append(0)
+        else:
+            self.stack.append((n1 // n2) % 2**256)
         return False
 
     def op_push1(self, bytecode):
@@ -50,12 +65,21 @@ class EVM:
                 self.consume_gas(gas_cost)
                 stop_execution = opcode_fn(bytecode)
             else:
-                raise Exception(f"Invalid opcode: {op}")
+                if 0x60 <= op <= 0x7f: 
+                    num_bytes = op - 0x5f
+                    value = int.from_bytes(bytecode[self.pc : self.pc + num_bytes], byteorder='big')
+                    self.stack.append(value)
+                    self.pc += num_bytes
+                else:
+                    raise Exception(f"Invalid opcode: {op}")
+
 
 if __name__ == "__main__":
     initial_gas = 1000
     evm = EVM(initial_gas)
-    bytecode = bytearray([0x60, 0x04, 0x60, 0x05, 0x01, 0x00])  # PUSH1 0x04, PUSH1 0x05, ADD, STOP
+    bytecode = bytearray(
+       [0x60, 0x05, 0x60, 0x05, 0x04, 0x00]
+    )
     evm.execute(bytecode)
     print(evm.stack)
     print(f"Remaining gas: {evm.gas}")
